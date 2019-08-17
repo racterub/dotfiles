0736167c99ff013cdd7d747b564879a5d7d57865
racterub <racterub@gmail.com>
2019-08-17 18:28:43 +0800

    fuck

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ if $UPGRADE; then
     echo "+==========================+"
     echo "|Updating & Upgrading..... |"
     echo "+==========================+"
-    sudo apt-get-get update
+    sudo apt-get update
     sudo apt-get -y upgrade
     sudo apt-get -y dist-upgrade
     sudo apt-get -y autoremove
